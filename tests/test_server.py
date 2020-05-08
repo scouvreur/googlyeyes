ed9b678a670f10b8300034745a9a676fd65ccbac
Stephane Couvreur <stephane.couvreur.sueron@gmail.com>
2020-05-08 11:21:35 +0100

    Change protocol name to scheme

--- tests/test_server.py
+++ tests/test_server.py
@@ -12,14 +12,14 @@ from googlyeyes.helper_functions import (
 )
 from googlyeyes.server import host, port
 
-protocol = "http://"
+scheme = "http://"
 
 
 def test_GET_imageUpload_endpoint():
     """Test for POST to /imageUpload endpoint
     """
     endpoint = "/imageUpload"
-    url = protocol + host + ":" + str(port) + endpoint
+    url = scheme + host + ":" + str(port) + endpoint
     response = requests.get(url)
     assert response.ok
     assert response.status_code == 200
@@ -30,7 +30,7 @@ def test_POST_imageUpload_endpoint():
     """Test for POST to /imageUpload endpoint
     """
     endpoint = "/imageUpload"
-    url = protocol + host + ":" + str(port) + endpoint
+    url = scheme + host + ":" + str(port) + endpoint
     response = POST_image(path="tests/data/test_payload_nface_1.jpeg", url=url)
     output_image = buffer_to_image(response.content)
     filename = response.headers["Content-Disposition"].split('=')[1]
@@ -56,13 +56,9 @@ def test_POST_test_endpoint():
     headers = {"content-type": "application/json"}
     payload = {"FirstName": "Stephane", "LastName": "Couvreur"}
     endpoint = "/test"
-    url = protocol + host + ":" + str(port) + endpoint
+    url = scheme + host + ":" + str(port) + endpoint
     response = requests.post(url, json=payload, headers=headers)
     assert response.ok
     assert response.status_code == 200
     assert response.json()["status"] == "success"
     assert response.json()["message"] == "Request from Stephane Couvreur"
-
-
-endpoint = "/imageUpload"
-url = protocol + host + str(port) + endpoint
